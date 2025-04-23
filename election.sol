dae4cad16bd98aa08c04acd3219d2abc9fd00858
Varun <140383096+RathoreVarun@users.noreply.github.com>
2025-04-23 19:25:45 +0530

    Update election.sol

--- election.sol
+++ election.sol
@@ -39,4 +39,13 @@ contract DecentralizedElection {
         require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate");
         return candidates[_candidateId].voteCount;
     }
+
+    // Get all candidates
+    function getAllCandidates() public view returns (Candidate[] memory) {
+        Candidate[] memory candidateList = new Candidate[](candidatesCount);
+        for (uint i = 1; i <= candidatesCount; i++) {
+            candidateList[i - 1] = candidates[i];
+        }
+        return candidateList;
+    }
 }
